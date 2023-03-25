5c4ba5d9f6fa7a24998238d83b84e8b177eec5a0
jeevarajr <123170284+jeevarajr@users.noreply.github.com>
2023-03-25 13:57:58 +0530

    Create StaticvsDynamic.cpp

---
+++ C++ Basics/Arrays/StaticvsDynamic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int *p; // creating a pointer to allocate memory dynamically
+p = new int[5]; //dynamic array creation
+p[0]=10;
+p[1]=20;
+p[2]=30;
+cout<<p[1]<<endl;
+delete [] p; //deleting dynamic memory
+cout<<p[1]; //wil print some garbage value 
+return 0;
+}
