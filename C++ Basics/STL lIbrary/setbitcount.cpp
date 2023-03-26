596452f25a508e411a284f91d3b621f730132026
Jeevaraj R <123170284+jeevarajr@users.noreply.github.com>
2023-03-26 12:58:34 +0530

    Create setbitcount.cpp

---
+++ C++ Basics/STL lIbrary/setbitcount.cpp
@@ -0,0 +1,13 @@
+//Set bit count
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {  
+
+int a=7;// 111 how many 1s are there. set bits 
+cout<<__builtin_popcount(a); //inbuilt function
+    return 0;
+}
+
+OUTPUT:
+3
