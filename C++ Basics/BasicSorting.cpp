28a045b6b6e0f222fa1fc7a1634fb87362409fd9
Jeevaraj R <123170284+jeevarajr@users.noreply.github.com>
2023-04-01 19:09:20 +0530

    Update BasicSorting.cpp

--- C++ Basics/BasicSorting.cpp
+++ C++ Basics/BasicSorting.cpp
@@ -1,1 +1,69 @@
-https://www.jdoodle.com/embed/v0/65nl
+#include <iostream>
+
+using namespace std;
+
+void selection_sort(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int min=i;
+        for(int j=i;j<=n;j++)
+        {
+            if(arr[j]<arr[min]){
+                int temp=arr[j];
+                arr[j]=arr[min];
+                arr[min]=temp;
+            }
+        }
+    }
+}
+
+void bubble_sort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                
+            }
+        }
+    }
+}
+void insertion_sort(int arr[],int n)
+{
+  for(int i=0;i<n;i++)
+  {
+      int j=i;
+      while(j>0 and arr[j-1]>arr[j])
+      {
+          int temp =arr[j-1];
+          arr[j-1]=arr[j];
+          arr[j]=temp;
+          j++;
+      }
+  }
+}
+int main() {
+    cout<<"Enter size of array";
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cout<<"Enter Array element no"<<i<<" ";
+        cin>>arr[i];
+    }
+   
+    insertion_sort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
