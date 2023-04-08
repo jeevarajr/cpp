d3990cf4f3a436e246b5767635e13682bf655b61
Jeevaraj R <123170284+jeevarajr@users.noreply.github.com>
2023-04-08 18:26:30 +0530

    Create 4PrintPractice.cpp

---
+++ EssentialC++Skills/4PrintPractice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"    1\n"<<"   232\n"<<"  34543\n"<<" 4567654\n"<<"567898765\n";
+    return 0;
+}
+
+OUTPUT:
+    1
+   232
+  34543
+ 4567654
+567898765
